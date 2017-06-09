ef7a43b629143e062bf3adc040842cfcd5902f24
shreyashah115 <shreyashah115@gmail.com>
2017-06-09 13:59:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -44,6 +44,6 @@ app.put('/contactlist/:id', function (req, res) {
     }
   );
 });;
-app.listen(3000);
-console.log("server running on port 3000");
+app.listen(process.env.PORT ||3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
